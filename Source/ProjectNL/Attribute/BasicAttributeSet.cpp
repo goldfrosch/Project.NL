f9612b5f35380961858ccc6fe84d30148c51f89e
GoldFrosch <goldfrosch@daum.net>
2024-07-09 23:34:54 +0900

    feat: attribute 속성에 대한 반응 function 추가

--- Source/ProjectNL/Attribute/BasicAttributeSet.cpp
+++ Source/ProjectNL/Attribute/BasicAttributeSet.cpp
@@ -2,3 +2,41 @@
 
 
 #include "BasicAttributeSet.h"
+#include "Net/UnrealNetwork.h"
+
+
+void UBasicAttributeSet::OnRepHealth(const FGameplayAttributeData& OldHealth) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Health, OldHealth);
+}
+
+void UBasicAttributeSet::OnRepMaxHealth(const FGameplayAttributeData& OldMaxHealth) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UBasicAttributeSet::OnRepMana(const FGameplayAttributeData& OldMana) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Mana, OldMana);
+}
+
+void UBasicAttributeSet::OnRepMaxMana(const FGameplayAttributeData& OldMaxMana) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UBasicAttributeSet::OnRepStamina(const FGameplayAttributeData& OldStamina) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Stamina, OldStamina);
+}
+
+void UBasicAttributeSet::OnRepMaxStamina(const FGameplayAttributeData& OldMaxStamina) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldMaxStamina);
+}
+
+void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
--- Source/ProjectNL/Attribute/BasicAttributeSet.h
+++ Source/ProjectNL/Attribute/BasicAttributeSet.h
@@ -20,27 +20,48 @@ class PROJECTNL_API UBasicAttributeSet : public UAttributeSet
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepHealth)
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Health)
 
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepMaxHealth)
 	FGameplayAttributeData MaxHealth;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, MaxHealth)
 	
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepMana)
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Mana)
 
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepMaxMana)
 	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, MaxMana)
 	
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepStamina)
 	FGameplayAttributeData Stamina;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Stamina)
 
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly, Category="Attributes", ReplicatedUsing = OnRepMaxStamina)
 	FGameplayAttributeData MaxStamina;
 	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, MaxStamina)
+
+	// 임시 속성으로 주기적인 업데이트가 필요한 속성은 아니라 OnRep를 할당하지 않음
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Damage)
+
+	UFUNCTION()
+	virtual void OnRepHealth(const FGameplayAttributeData& OldHealth);
+	UFUNCTION()
+	virtual void OnRepMaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	UFUNCTION()
+	virtual void OnRepMana(const FGameplayAttributeData& OldMana);
+	UFUNCTION()
+	virtual void OnRepMaxMana(const FGameplayAttributeData& OldMaxMana);
+	UFUNCTION()
+	virtual void OnRepStamina(const FGameplayAttributeData& OldStamina);
+	UFUNCTION()
+	virtual void OnRepMaxStamina(const FGameplayAttributeData& OldMaxStamina);
+
+	// TODO: 복제에 도움이 되는 함수지만 정확한 기능은 추후
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 };
