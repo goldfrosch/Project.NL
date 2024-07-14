174fddffd88e90fa7607f5c8ce17478d68a16c87
GoldFrosch <goldfrosch@daum.net>
2024-07-14 21:26:21 +0900

    chore: add mana and stamina

--- Source/ProjectNL/Attribute/BasicAttributeSet.cpp
+++ Source/ProjectNL/Attribute/BasicAttributeSet.cpp
@@ -13,21 +13,21 @@ void UBasicAttributeSet::OnRepMaxHealth(const FGameplayAttributeData& OldMaxHeal
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxHealth, OldMaxHealth);
 }
 
-// void UBasicAttributeSet::OnRepMana(const FGameplayAttributeData& OldMana) {
-// 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Mana, OldMana);
-// }
-//
-// void UBasicAttributeSet::OnRepMaxMana(const FGameplayAttributeData& OldMaxMana) {
-// 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxMana, OldMaxMana);
-// }
-//
-// void UBasicAttributeSet::OnRepStamina(const FGameplayAttributeData& OldStamina) {
-// 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Stamina, OldStamina);
-// }
-//
-// void UBasicAttributeSet::OnRepMaxStamina(const FGameplayAttributeData& OldMaxStamina) {
-// 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldMaxStamina);
-// }
+void UBasicAttributeSet::OnRepMana(const FGameplayAttributeData& OldMana) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Mana, OldMana);
+}
+
+void UBasicAttributeSet::OnRepMaxMana(const FGameplayAttributeData& OldMaxMana) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UBasicAttributeSet::OnRepStamina(const FGameplayAttributeData& OldStamina) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Stamina, OldStamina);
+}
+
+void UBasicAttributeSet::OnRepMaxStamina(const FGameplayAttributeData& OldMaxStamina) {
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldMaxStamina);
+}
 
 void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
@@ -35,8 +35,8 @@ void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& O
 
 	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Health, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
-	// DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Mana, COND_None, REPNOTIFY_Always);
-	// DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
-	// DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
-	// DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
 }
