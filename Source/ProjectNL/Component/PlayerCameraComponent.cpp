47acbe6aac277606178848b2004ba46cfea076fe
GoldFrosch <goldfrosch@daum.net>
2024-07-25 13:20:54 +0900

    refactor: Camera 관련 로직 분리

---
+++ Source/ProjectNL/Component/PlayerCameraComponent.cpp
@@ -0,0 +1,47 @@
+﻿#include "PlayerCameraComponent.h"
+
+#include "GameFramework/SpringArmComponent.h"
+
+UPlayerCameraComponent::UPlayerCameraComponent()
+{
+	CameraSpring = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpring"));
+	CameraSpring->TargetArmLength = CameraZoom;
+	CameraSpring->bUsePawnControlRotation = true;
+	
+	FirstCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstCamera"));
+	FirstCamera->bUsePawnControlRotation = true;
+	
+	ThirdFollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ThirdFollowCamera"));
+	ThirdFollowCamera->bUsePawnControlRotation = false;
+}
+
+
+void UPlayerCameraComponent::ToggleCamera(const FInputActionValue& Value)
+{
+	if (CameraStatus != EPlayerCameraStatus::First && CameraStatus != EPlayerCameraStatus::Third) return;
+	
+	if (CameraStatus == EPlayerCameraStatus::Third)
+	{
+		CameraSpring->TargetArmLength = 0;
+		SetFirstPersonView();
+		CameraStatus = EPlayerCameraStatus::First;
+	} else
+	{
+		CameraStatus = EPlayerCameraStatus::Third;
+		CameraSpring->TargetArmLength = CameraZoom;
+		SetThirdPersonView();
+	}
+	OnPlayerCameraModeChanged.Broadcast(CameraStatus);
+}
+
+void UPlayerCameraComponent::SetFirstPersonView()
+{
+	FirstCamera->SetActive(true);
+	ThirdFollowCamera->SetActive(false);
+}
+
+void UPlayerCameraComponent::SetThirdPersonView()
+{
+	FirstCamera->SetActive(false);
+	ThirdFollowCamera->SetActive(true);
+}
