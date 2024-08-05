47811e2ee46cb8a37ded76714b9342e95a0fab66
GoldFrosch <goldfrosch@daum.net>
2024-08-05 16:53:55 +0900

    feat: 시점 고정 타겟팅 고도화

--- Source/ProjectNL/Component/PlayerCameraComponent.cpp
+++ Source/ProjectNL/Component/PlayerCameraComponent.cpp
@@ -25,7 +25,6 @@ void UPlayerCameraComponent::TickComponent(float DeltaTime, ELevelTick TickType
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	if (IsValid(TargetActor))
 	{
-		UE_LOG(LogTemp, Display, TEXT("A: %s"), *GetOwner()->GetName());
 		if (APlayerCharacter* Owner = Cast<APlayerCharacter>(GetOwner()))
 		{
 			const FRotator ViewTo = UKismetMathLibrary::FindLookAtRotation(
