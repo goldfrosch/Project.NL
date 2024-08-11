db2c0e01355c37b8704383108d89ef802aec2529
GoldFrosch <goldfrosch@daum.net>
2024-08-11 18:31:33 +0900

    refactor: Move, Look GA 마이그레이션

---
+++ Source/ProjectNL/GAS/Ability/Characters/Active/Move/Move.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Move.h"
+
+#include "AbilitySystemComponent.h"
+#include "InputActionValue.h"
+#include "ProjectNL/Helper/StateHelper.h"
+#include "ProjectNL/Manager/MovementManager.h"
+
+UMove::UMove(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bCancelAbilityOnInputReleased = true;
+}
+
+void UMove::OnTriggeredInputAction(const FInputActionValue& Value)
+{
+	if (FStateHelper::IsPlayerIdle(GetAbilitySystemComponentFromActorInfo()))
+	{
+		const FVector2D MovementVector = Value.Get<FVector2D>();
+		UMovementManager::Move(Cast<APawn>(GetAvatarActorFromActorInfo())
+													, MovementVector);
+	}
+}
