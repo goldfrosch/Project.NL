9e8f1d72cde96fd254ceb0aec39ef872aa5cd361
GoldFrosch <goldfrosch@daum.net>
2024-08-24 10:41:19 +0900

    feat: 테스트용 shield instance

---
+++ Source/ProjectNL/Weapon/Shield/ShieldBase.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectNL/Weapon/WeaponBase.h"
+#include "ShieldBase.generated.h"
+
+UCLASS()
+class PROJECTNL_API AShieldBase : public AWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AShieldBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
