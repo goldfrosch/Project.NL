7d85f27a3efb2fee7105dbe6e760e926ae899e1f
GoldFrosch <goldfrosch@daum.net>
2024-08-24 10:41:00 +0900

    fix: 테스트용 delegate 제거

--- Source/ProjectNL/GAS/Ability/Characters/Active/ComboAttack/ComboAttack.h
+++ Source/ProjectNL/GAS/Ability/Characters/Active/ComboAttack/ComboAttack.h
@@ -52,11 +52,6 @@ private:
 	void HandleComboNotifyEnd(const EHandEquipStatus AttackHand);
 
 	UFUNCTION()
-	void Damage(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor
-							, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex
-							, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
 	void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
 
 	UFUNCTION()
